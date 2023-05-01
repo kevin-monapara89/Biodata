dd6f87099316878d63038f3fb087a24fc4307476
kevin-monapara89 <kevinmonapara89@gmail.com>
2023-05-01 19:18:00 +0530

    commit

--- app/src/main/java/com/kevin/biodata/MainActivity.java
+++ app/src/main/java/com/kevin/biodata/MainActivity.java
@@ -1,13 +1,28 @@
 package com.kevin.biodata;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnsubmit;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnsubmit = findViewById(R.id.btnsubmit);
+
+        btnsubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Submit Succesfull!!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
